c937cdd9ec7bd6107ba491cfce7db58f961abdff
Erica Irving <contact.eirving@gmail.com>
2015-07-13 13:16:37 -0400

    it;s a little prettier and mor reliable re previous

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 $("document").ready(function () {
 
-var clickCount = 0;
-var cardsToCompare = [];
+  var clickCount = 0;
 
   $(".card").on ("click", function () {
-    $(this).removeClass("back");
-    $(this).addClass("front");
-    cardsToCompare.push(document.getElementsByClassName("front")[clickCount].children[0].id);
+    $(this).removeClass("back").addClass("front active");
     clickCount++;
-    console.log(cardsToCompare)
     if(clickCount == 2) {
-      cardsAsString = cardsToCompare.toString()
-      console.log(cardsAsString);
-      if(cardsAsString.substring(0,1) !== cardsAsString.substring(3,4)) {
+      if($(".active span").eq(0).html() !== $(".active span").eq(1).html()) {
         setTimeout(function(){
-          $(".card").addClass("back");
-          }, 3000)
-          clickCount = 0;
-        }
-      else {
-        $(".front").on("click", function(){return false;})
+          $(".active").addClass("back").removeClass("front active");
+        }, 2000)
+          //clickCount = 0;
       }
-      cardsToCompare = [];
-    }
+      else if ($(".active span").eq(0).html() === $(".active span").eq(1).html()){
+        $(".front").on("click", function(){return false;}).removeClass("active");
+        //clickCount = 0;
+      }
+      clickCount = 0;
+   }
   })
+
+
+
+
+
+
+
 
 })
