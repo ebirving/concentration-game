941f582b9617f973ef93136b913411c0c70b327d
Erica Irving <contact.eirving@gmail.com>
2015-07-13 11:25:53 -0400

    Two flips per turn

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 $("document").ready(function () {
-
+  //As a user, I want to click cards to turn them face-up so I can look for matches.
+  //As a user, I want to turn over two cards per turn so I can see if they match.
+  var clickCount = 0
   $(".card").on ("click", function () {
-    console.log("QUOI?");
     $(this).removeClass("back");
+    clickCount++;
+    console.log(clickCount);
+    if(clickCount == 2) {
+      setTimeout(function(){
+        $(".card").addClass("back");
+      }, 3000)
+    clickCount = 0;
+    }
   })
-
 })
