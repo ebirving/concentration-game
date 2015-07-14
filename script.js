e9e024fba7fcb54a52a5dd33a41e80c6a483c643
Erica Irving <contact.eirving@gmail.com>
2015-07-13 20:09:28 -0400

    Minor styling tweaks

--- script.js
+++ script.js
@@ -27,3 +27,12 @@ $("#new_game").on ("click", function () {
 
 
 })
+
+// function shuffleArray(array) {
+//    for (var i = array.length - 1; i > 0; i--) {
+//        var j = Math.floor(Math.random() * (i + 1));
+//        var temp = array[i];
+//        array[i] = array[j];
+//        array[j] = temp;
+//    }
+//    return array;
