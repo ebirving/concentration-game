ae7949d1b34e9e05b90383fee0986a8b69cde384
Erica Irving <contact.eirving@gmail.com>
2015-07-14 10:04:08 -0400

    fuction shuffleDeck is functional on load and on clicking new game button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-$("document").ready(function () {
+// $("document").ready(function () {
 
-  var clickCount = 0;
+var clickCount = 0;
 
 var spaceIcons = [
     rocketshipOne = {src: "space_icons/rocketship.svg", alt: "Rocket Ship"},
@@ -8,7 +8,7 @@ var spaceIcons = [
     ringedPlanetOne = {src: "space_icons/rocketship.svg", alt: "Ringed Planet"},
     ringedPlanetTwo = {src: "space_icons/rocketship.svg", alt: "Ringed Planet"},
     constellationOne = {src: "space_icons/constellation.svg", alt: "Constellation"},
-    constellationTwo: = {src: "space_icons/constellation.svg", alt: "Constellation"},
+    constellationTwo = {src: "space_icons/constellation.svg", alt: "Constellation"},
     spacesuitOne = {src: "space_icons/spacesuit.svg", alt: "Spacesuit"},
     spacesuitTwo = {src: "space_icons/spacesuit.svg", alt: "Spacesuit"},
     stripedPlanetOne = {src: "space_icons/striped_planet.svg", alt: "Striped Planet"},
@@ -21,37 +21,42 @@ var spaceIcons = [
     telescopeTwo = {src: "space_icons/telescope.svg", alt: "Telescope"},
   ]
 
-  function shuffleArray(array) {
-     for (var i = array.length - 1; i > 0; i--) {
-         var j = Math.floor(Math.random() * (i + 1));
-         var temp = array[i];
-         array[i] = array[j];
-         array[j] = temp;
-     }
-     return array;
-
-  $(".card").on ("click", function () {
-    $(this).removeClass("back").addClass("front guessed");
-    clickCount++;
-    if(clickCount == 2) {
-      if($(".guessed img").eq(0).attr("src") !== $(".guessed img").eq(1).attr("src")) {
-        setTimeout(function(){
-          $(".guessed").addClass("back").removeClass("front guessed");
-        }, 1000)
-          //clickCount = 0;
-      }
-      else if ($(".guessed span").eq(0).html() === $(".guessed span").eq(1).html()){
-        $(".guessed").removeClass("guessed");
-        //clickCount = 0;
-      }
-      clickCount = 0;
+function shuffleDeck(deck) {
+   for (var i = deck.length - 1; i > 0; i--) {
+       var j = Math.floor(Math.random() * (i + 1));
+       var temp = deck[i];
+       deck[i] = deck[j];
+       deck[j] = temp;
    }
-  })
+   //return deck;
+   console.log(deck);
+ }
+
+window.onload = shuffleDeck(spaceIcons)
+
+$(".card").on ("click", function () {
+  $(this).removeClass("back").addClass("front guessed");
+  clickCount++;
+  if(clickCount == 2) {
+    if($(".guessed img").eq(0).attr("src") !== $(".guessed img").eq(1).attr("src")) {
+      setTimeout(function(){
+        $(".guessed").addClass("back").removeClass("front guessed");
+      }, 1000)
+        //clickCount = 0;
+    }
+    else if ($(".guessed span").eq(0).html() === $(".guessed span").eq(1).html()){
+      $(".guessed").removeClass("guessed");
+      //clickCount = 0;
+    }
+    clickCount = 0;
+ }
+})
 
 $("#new_game").on ("click", function () {
-  $(".card").addClass("back").removeClass("front guessed")
+$(".card").addClass("back").removeClass("front guessed");
+shuffleDeck(spaceIcons);
 })
 
 
 
-})
+// })
