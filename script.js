bc9d7c17532371e5247d127081bbac59fa27ae39
Erica Irving <contact.eirving@gmail.com>
2015-07-14 09:49:50 -0400

    instantiated array of objects containing space icons

--- script.js
+++ script.js
@@ -2,6 +2,34 @@ $("document").ready(function () {
 
   var clickCount = 0;
 
+var spaceIcons = [
+    rocketshipOne = {src: "space_icons/rocketship.svg", alt: "Rocket Ship"},
+    rocketshipTwo = {src: "space_icons/rocketship.svg", alt: "Rocket Ship"},
+    ringedPlanetOne = {src: "space_icons/rocketship.svg", alt: "Ringed Planet"},
+    ringedPlanetTwo = {src: "space_icons/rocketship.svg", alt: "Ringed Planet"},
+    constellationOne = {src: "space_icons/constellation.svg", alt: "Constellation"},
+    constellationTwo: = {src: "space_icons/constellation.svg", alt: "Constellation"},
+    spacesuitOne = {src: "space_icons/spacesuit.svg", alt: "Spacesuit"},
+    spacesuitTwo = {src: "space_icons/spacesuit.svg", alt: "Spacesuit"},
+    stripedPlanetOne = {src: "space_icons/striped_planet.svg", alt: "Striped Planet"},
+    stripedPlanetTwo = {src: "space_icons/striped_planet.svg", alt: "Striped Planet"},
+    ufoOne = {src: "space_icons/ufo.svg", alt: "UFO"},
+    ufoTwo = {src: "space_icons/ufo.svg", alt: "UFO"},
+    satelliteOne = {src: "space_icons/satellite.svg", alt: "Satellite"},
+    satelliteTwo = {src: "space_icons/satellite.svg", alt: "Satellite"},
+    telescopeOne = {src: "space_icons/telescope.svg", alt: "Telescope"},
+    telescopeTwo = {src: "space_icons/telescope.svg", alt: "Telescope"},
+  ]
+
+  function shuffleArray(array) {
+     for (var i = array.length - 1; i > 0; i--) {
+         var j = Math.floor(Math.random() * (i + 1));
+         var temp = array[i];
+         array[i] = array[j];
+         array[j] = temp;
+     }
+     return array;
+
   $(".card").on ("click", function () {
     $(this).removeClass("back").addClass("front guessed");
     clickCount++;
@@ -27,12 +55,3 @@ $("#new_game").on ("click", function () {
 
 
 })
-
-// function shuffleArray(array) {
-//    for (var i = array.length - 1; i > 0; i--) {
-//        var j = Math.floor(Math.random() * (i + 1));
-//        var temp = array[i];
-//        array[i] = array[j];
-//        array[j] = temp;
-//    }
-//    return array;
