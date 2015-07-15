0babc523c114fd35f67538fd8128d755317d934f
Erica Irving <contact.eirving@gmail.com>
2015-07-15 10:01:42 -0400

    adjustments to spacing and formatting

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 var spaceIcons = [
-    rocketshipOne = {src: "space_icons/rocketship.svg", alt: "Rocket Ship"},
-    rocketshipTwo = {src: "space_icons/rocketship.svg", alt: "Rocket Ship"},
-    ringedPlanetOne = {src: "space_icons/ringed_planet.svg", alt: "Ringed Planet"},
-    ringedPlanetTwo = {src: "space_icons/ringed_planet.svg", alt: "Ringed Planet"},
-    constellationOne = {src: "space_icons/constellation.svg", alt: "Constellation"},
-    constellationTwo = {src: "space_icons/constellation.svg", alt: "Constellation"},
-    spacesuitOne = {src: "space_icons/spacesuit.svg", alt: "Spacesuit"},
-    spacesuitTwo = {src: "space_icons/spacesuit.svg", alt: "Spacesuit"},
-    stripedPlanetOne = {src: "space_icons/striped_planet.svg", alt: "Striped Planet"},
-    stripedPlanetTwo = {src: "space_icons/striped_planet.svg", alt: "Striped Planet"},
-    ufoOne = {src: "space_icons/ufo.svg", alt: "UFO"},
-    ufoTwo = {src: "space_icons/ufo.svg", alt: "UFO"},
-    satelliteOne = {src: "space_icons/satellite.svg", alt: "Satellite"},
-    satelliteTwo = {src: "space_icons/satellite.svg", alt: "Satellite"},
-    telescopeOne = {src: "space_icons/telescope.svg", alt: "Telescope"},
-    telescopeTwo = {src: "space_icons/telescope.svg", alt: "Telescope"},
-  ]
-var clickCount = 0;
+  rocketshipOne = {src: "space_icons/rocketship.svg", alt: "Rocket Ship"},
+  rocketshipTwo = {src: "space_icons/rocketship.svg", alt: "Rocket Ship"},
+  ringedPlanetOne = {src: "space_icons/ringed_planet.svg", alt: "Ringed Planet"},
+  ringedPlanetTwo = {src: "space_icons/ringed_planet.svg", alt: "Ringed Planet"},
+  constellationOne = {src: "space_icons/constellation.svg", alt: "Constellation"},
+  constellationTwo = {src: "space_icons/constellation.svg", alt: "Constellation"},
+  spacesuitOne = {src: "space_icons/spacesuit.svg", alt: "Spacesuit"},
+  spacesuitTwo = {src: "space_icons/spacesuit.svg", alt: "Spacesuit"},
+  stripedPlanetOne = {src: "space_icons/striped_planet.svg", alt: "Striped Planet"},
+  stripedPlanetTwo = {src: "space_icons/striped_planet.svg", alt: "Striped Planet"},
+  ufoOne = {src: "space_icons/ufo.svg", alt: "UFO"},
+  ufoTwo = {src: "space_icons/ufo.svg", alt: "UFO"},
+  satelliteOne = {src: "space_icons/satellite.svg", alt: "Satellite"},
+  satelliteTwo = {src: "space_icons/satellite.svg", alt: "Satellite"},
+  telescopeOne = {src: "space_icons/telescope.svg", alt: "Telescope"},
+  telescopeTwo = {src: "space_icons/telescope.svg", alt: "Telescope"},
+]
 
 function resetGame() {
+  clickCount = 0;
+  //console.log(clickCount)
+  $(".back").on("click", flipCard);
   $(".card").addClass("back").removeClass("front guessed");
   $(".card img").remove();
   shuffleDeck(spaceIcons);
-  $(".back").on("click", flipCard);
 }
 
 function shuffleDeck(deck) {
@@ -31,7 +32,7 @@ function shuffleDeck(deck) {
     var temp = deck[i];
     deck[i] = deck[j];
     deck[j] = temp;
-    $(".card").eq(i).append("<img src=" + deck[i].src + " alt=" + deck[i].alt + ">")
+    $(".card").eq(i).append("<img src=" + deck[i].src + " alt=" + deck[i].alt + ">");
   }
 }
 
@@ -45,8 +46,7 @@ function flipCard() {
     compareCards();
     clickCount = 0;
     setTimeout(function (){
-      $(".back").on("click", flipCard);
-    }, 1000);
+      $(".back").on("click", flipCard)}, 1000);
   }
 }
 
@@ -58,9 +58,10 @@ function compareCards() {
   }
   else if (guessOne !== guessTwo) {
     setTimeout(function () {
-    $(".guessed").addClass("back").removeClass("front guessed")}, 1000)
+    $(".guessed").addClass("back").removeClass("front guessed")}, 1000);
   }
 }
 
 $(window).on("load", resetGame)
 $("#new_game").on("click", resetGame)
+//semicolons and indents
